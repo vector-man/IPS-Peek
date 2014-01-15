7f413539d5788d8b08958b99f72ca0dd41cda5cf
Michael <admin@codeisle.com>
2014-01-14 19:12:10 -0500

    Created new IPS element classes.

---
+++ IpsPeek/IpsLibNet/Patching/IpsResizeElement.cs
@@ -0,0 +1,27 @@
+﻿using IpsLibNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace IpsPeek.IpsLibNet.Patching
+{
+    [DisplayName("Resize")]
+    public class IpsResizeElement : IpsElement
+    {
+        private int _size;
+        public IpsResizeElement(int size, Range ipsFileRange, int ipsFileSize, byte[] data)
+            : base(ipsFileRange, ipsFileSize, data)
+        {
+            _size = size;
+        }
+        public int Size
+        {
+            get
+            {
+                return _size;
+            }
+        }
+    }
+}
