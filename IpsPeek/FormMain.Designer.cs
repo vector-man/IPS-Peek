bd9e232e511c955c690c82902d6bfa4913d076e2
Michael <admin@codeisle.com>
2014-01-20 13:29:52 -0500

    Added code to close the application when the 'Exit' menu item is clicked.

--- IpsPeek/FormMain.Designer.cs
+++ IpsPeek/FormMain.Designer.cs
@@ -140,6 +140,7 @@
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
             this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
             // viewToolStripMenuItem
             // 
