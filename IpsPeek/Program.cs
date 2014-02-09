086957f494e1c8455147b2c5a40278ad8656088b
Michael <admin@codeisle.com>
2014-02-09 18:07:19 -0500

    Added event handler for unhandled exceptions.

--- IpsPeek/Program.cs
+++ IpsPeek/Program.cs
@@ -14,9 +14,26 @@ namespace IpsPeek
         [STAThread]
         static void Main()
         {
+           // AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                Exception ex = (Exception)e.ExceptionObject;
+
+                MessageBox.Show("Whoops! Please contact the developers with the following"
+                      + " information:\n\n" + ex.Message + ex.StackTrace,
+                      "Fatal Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            finally
+            {
+                Application.Exit();
+            }
         }
     }
 }
