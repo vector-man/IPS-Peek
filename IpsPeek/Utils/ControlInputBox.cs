b20b25788ce141dc61391f323b7250efeb1f7933
Michael <admin@codeisle.com>
2014-04-10 00:36:02 -0400

    Added 'ControlInputBox' to project.

---
+++ IpsPeek/Utils/ControlInputBox.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IpsPeek.Utils
+{
+   public class ControlInputBox
+    {
+        /// <summary>
+        /// Displays a dialog with a prompt and textbox where the user can enter information
+        /// </summary>
+        /// <param name="title">Dialog title</param>
+        /// <param name="promptText">Dialog prompt</param>
+        /// <param name="value">Sets the initial value and returns the result</param>
+        /// <returns>Dialog result</returns>
+        public static DialogResult Show(IWin32Window owner, string title, string promptText, Control inputControl)
+        {
+            Form form = new Form();
+            Label label = new Label();
+            Button buttonOk = new Button();
+            Button buttonCancel = new Button();
+
+            form.Text = title;
+            label.Text = promptText;
+
+            buttonOk.Text = "OK";
+            buttonCancel.Text = "Cancel";
+            buttonOk.DialogResult = DialogResult.OK;
+            buttonCancel.DialogResult = DialogResult.Cancel;
+
+            label.SetBounds(9, 20, 372, 13);
+            inputControl.SetBounds(12, 36, 372, 20);
+            buttonOk.SetBounds(228, 72, 75, 23);
+            buttonCancel.SetBounds(309, 72, 75, 23);
+
+            label.AutoSize = true;
+            inputControl.Anchor = inputControl.Anchor | AnchorStyles.Right;
+            buttonOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            buttonCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+
+            form.ClientSize = new Size(396, 107);
+            form.Controls.AddRange(new Control[] {
+			label,
+			inputControl,
+			buttonOk,
+			buttonCancel
+		});
+            form.ClientSize = new Size(Math.Max(300, label.Right + 10), form.ClientSize.Height);
+            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form.StartPosition = FormStartPosition.CenterParent;
+            form.MinimizeBox = false;
+            form.MaximizeBox = false;
+            form.AcceptButton = buttonOk;
+            form.CancelButton = buttonCancel;
+
+            DialogResult dialogResult = form.ShowDialog(owner);
+            return dialogResult;
+        }
+    }
+}
