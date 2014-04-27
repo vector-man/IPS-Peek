8fd05b68c206b387e00a4079f73f50f60e4689de
Michael <admin@codeisle.com>
2014-04-27 11:21:27 -0400

    Added "GoToType" for "GoToHexBoxDialog."

---
+++ IpsPeek/GoToType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IpsPeek
+{
+    public enum GoToType
+    {
+        Decimal = 10,
+        Hexadecimal = 16,
+        Octal = 8
+    }
+}
