30908640aeeddbf671087edd002f45bf90ac8000
Michael <admin@codeisle.com>
2014-05-20 16:31:02 -0400

    Added HexBox project to solution.

---
+++ Be.Windows.Forms.HexBox/HighlightComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Be.Windows.Forms
+{
+    class HighlightComparer: IComparer<Highlight>
+    {
+        public int Compare(Highlight x, Highlight y)
+        {
+             // Check if value is below range (less than min).
+            if (x.Offset.CompareTo(y.Offset) > 0)
+            {
+                return 1;
+            }
+
+            // Check if value is below range (less than min).
+            if ((x.Length + x.Offset).CompareTo(y.Length + y.Offset) < 0)
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+}
