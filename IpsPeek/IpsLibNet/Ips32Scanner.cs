3cea74116655be824e72cc6506abc38e86f01dd4
Michael Corbett <factor44@hotmail.com>
2023-04-04 20:57:02 -0400

    Refactored classes. Added some Ips32Scanner class (unfinished).

--- IpsPeek/IpsLibNet/Ips32Scanner.cs
+++ IpsPeek/IpsLibNet/Ips32Scanner.cs
@@ -10,9 +10,17 @@ using System.Linq;
 
 namespace IpsLibNet
 {
-    public class IpsScanner
+    public class Ips32Scanner
     {
-        public string IpsHeader = "PATCH";
+        private bool _isIps32Patch;
+        public string Ips32Header = "IPS32";
+        private IpsScanner _ipsScanner;
+
+        public Ips32Scanner(IpsScanner scanner)
+        {
+            _ipsScanner = scanner;
+        }
+
         public int ToInteger(byte[] data)
         {
             if ((BitConverter.IsLittleEndian))
@@ -33,8 +41,6 @@ namespace IpsLibNet
 
         public List<IpsElement> Scan(Stream patch)
         {
-            patch.Seek(0, SeekOrigin.Begin);
-
             List<IpsElement> patches = new List<IpsElement>();
 
 
@@ -46,15 +52,12 @@ namespace IpsLibNet
 
             // Read 'PATCH' bytes of present (if not present, this is not a valid IPS patch).
             var patchHeaderBytes = Read(patch, 5, 0, 5);
-            var patchHeaderString = System.Text.ASCIIEncoding.ASCII.GetString(patchHeaderBytes);
+            var patchHeaderString = System.Text.Encoding.ASCII.GetString(patchHeaderBytes);
             // Check header for 'PATCH.'
             // If patch is not valid...
-            if (patchHeaderString != IpsHeader)
-            {
-                // Throw exception because 'PATCH' was not found in header.
-                throw new UnsupportedFileTypeException("The IPS patch format is not valid.", null);
-            }
-            
+            if (patchHeaderString != Ips32Header) return _ipsScanner.Scan(patch);
+
+            _isIps32Patch = (Ips32Header == patchHeaderString);
             patches.Add(new IpsIdValueElement(0));
             // Valid patch file, continue...
 
@@ -78,11 +81,11 @@ namespace IpsLibNet
             long patchStreamLength = patch.Length;
             while (!endOfFile && patch.Position < patchStreamLength)
             {
-                // Add 3 bytes from patch stream to data (potentially containing 'EOF').
-                var data = Read(patch, 4, 1, 3);
+                // Add 4 bytes from patch stream to data (potentially containing 'EEOF').
+                var data = Read(patch, 4, 1, 4);
 
                 // If not the end of the file (no 'EOF' string found).
-                if (!(System.Text.ASCIIEncoding.ASCII.GetString(data, 1, 3) == "EOF"))
+                if (!(System.Text.ASCIIEncoding.ASCII.GetString(data, 1, 3) == "EEOF"))
                 {
                     // Set offset to big-endian integer representation of offset bytes (taken from eof).
                     offset = ToInteger(data);
@@ -127,7 +130,7 @@ namespace IpsLibNet
                 else
                 {
                     endOfFile = true;
-                    patches.Add(new IpsEndOfFileValueElement((int)(patch.Position - 3)));
+                    patches.Add(new IpsEndOfFileValueElement((int)(patch.Position - 4)));
                 }
             }
 
@@ -141,7 +144,7 @@ namespace IpsLibNet
             try
             {
                 // Read 3 bytes from patch stream into data (potentially containing truncate information).
-                var truncate = Read(patch, 3, 0, 3);
+                var truncate = Read(patch, 4, 0, 3);
                 patches.Add(new IpsResizeValueElement((int)(patch.Position - 3), truncate));
             }
             catch
@@ -168,3 +171,127 @@ namespace IpsLibNet
     }
 
 }
+
+
+/* 
+ 	byte[] data = new byte[4];
+		using (System.IO.BinaryReader patch = new System.IO.BinaryReader(_patchStream)) {
+			try {
+				// Check header for 'PATCH'.
+
+				// If patch is not valid (should be set to the word PATCH).
+				if ((!(System.Text.ASCIIEncoding.ASCII.GetString(patch.ReadBytes(5)) == "PATCH"))) {
+					// Throw exception because 'PATCH' was not found in header.
+					throw new Exceptions.UnsupportedFileTypeException("IPS patch file format is not valid.", null);
+
+					// Else, valid patch file, continue.
+
+				} else {
+					// Add Id patch.
+					patches.Add(new IpsPatch(null, null, new Range((_patchStream.Position - 5), (_patchStream.Position - 1)), 5, IPSLibNet.IpsPatchType.Id));
+
+					int offset = 0;
+					// 3 bytes, gives offset into base file.
+					int size = 0;
+					// 2 bytes, size of next record data, is 0 if RLE.
+					bool endOfFile = false;
+					// Used see if EOF is found.
+
+					//rle stuff
+					int rleCount = 0;
+					// 2 bytes, the numbero f times to repeat the RLE byte.
+					byte rleByte = 0;
+					// The byte to repeat
+
+					int truncate = 0;
+					// Used for truncation data.
+
+					int patchCount = 0;
+
+
+					while (!endOfFile) {
+						// Patch record code below ... 
+
+						// Add 3 bytes from patch stream to data (potentially containing 'EOF').
+						data(1) = (patch.ReadByte);
+						data(2) = (patch.ReadByte);
+						data(3) = (patch.ReadByte);
+
+						// If not the end of the file (no 'EOF' string found).
+
+						if (!(System.Text.ASCIIEncoding.ASCII.GetString(data, 1, 3) == "EOF")) {
+							// Set offset to big-endian integer representation of offset bytes (taken from EOF).
+							offset = GetCorrectInteger(data);
+
+							// Clear data.
+							Array.Clear(data, 0, 4);
+
+							// Add 2 bytes from patch stream to data.
+							data(2) = (patch.ReadByte());
+							data(3) = (patch.ReadByte());
+
+							// Set size to big-endian integer representation of size bytes.
+							size = GetCorrectInteger(data);
+
+							// Clear data.
+							Array.Clear(data, 0, 4);
+
+							// If RLE patching.
+
+							if (size == 0) {
+								// Add 2 bytes from patch stream to data.
+								data(2) = patch.ReadByte;
+								data(3) = patch.ReadByte;
+
+								// Clear data.
+								Array.Clear(data, 0, 4);
+								// Read RLE byte
+								patch.ReadByte();
+
+								patches.Add(new IpsPatch(offset, size, new Range((_patchStream.Position - 8), (_patchStream.Position - 1)), 8, IPSLibNet.IpsPatchType.RlePatch));
+
+								// Else, No RLE, so use normal patching.
+							} else {
+								patch.ReadBytes(size);
+								patches.Add(new IpsPatch(offset, size, new Range(((_patchStream.Position - 10) - (size - 5)), (_patchStream.Position - 1)), (size + 5), IPSLibNet.IpsPatchType.NormalPatch));
+
+							}
+							// Else, it is the end of the file
+
+						} else {
+							endOfFile = true;
+
+							patches.Add(new IpsPatch(null, null, new Range((_patchStream.Position - 3), _patchStream.Position - 1), 3, IPSLibNet.IpsPatchType.Eof));
+
+
+							// Check for LunarIPS truncate command.
+							try {
+								// Add 3 bytes from patch stream to data (potentially containing truncate information).
+								data(1) = patch.ReadByte;
+								data(2) = patch.ReadByte;
+								data(3) = patch.ReadByte;
+
+								// Set truncate to the big-endian integer representation of the number of bytes to truncate stream.
+								truncate = GetCorrectInteger(data);
+
+								// Clear data
+								Array.Clear(data, 0, 4);
+
+								// If truncate data is right size.
+								if ((truncate > 0)) {
+
+									patches.Add(new IpsPatch(truncate, null, new Range((_patchStream.Position - 3), (_patchStream.Position - 1)), 3, IPSLibNet.IpsPatchType.Truncate));
+
+								}
+							} catch (Exception ex) {
+								// Not a truncate patch, no need for exception, just silently ignore.
+							}
+						}
+					}
+				}
+			} catch (Exception ex) {
+				throw ex;
+			}
+		}
+		return patches;
+ */
---
+++ IpsPeek/IpsLibNet/Patching/Ips32EndOfFileValueElement.cs
@@ -0,0 +1,18 @@
+﻿using IpsLibNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace IpsPeek.IpsLibNet.Patching
+{
+    public class Ips32EndOfFileValueElement : IpsValueElement
+    {
+        const string _value = "EEOF";
+        public Ips32EndOfFileValueElement(int ipsOffset)
+            : base(ipsOffset, _value.Length, Encoding.ASCII.GetBytes(_value))
+        {
+        }
+    }
+}
